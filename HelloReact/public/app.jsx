1781a864df6b0d7230cc116b33c6dbaf42b822f4
geekankit318 <geekankit318@gmail.com>
2017-06-17 22:48:16 +0530

    Added props and getDefaultProps

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,16 +1,31 @@
 var Greeter = React.createClass({
-    render : () => {
+
+    // Setting default props
+    getDefaultProps : function() {
+        return {
+            name : 'React',
+            message : 'This is form component'
+        };
+    },
+    render : function() {
+        // Pulling props via 'this.props' objects
+        var name = this.props.name;
+        var message = this.props.message;
         return (
             <div>
-                <h1>Hello React!</h1>
+                <h1>Hello {name}!</h1>
                 <h2>This is my first react app..</h2>
-                <p>This is form component</p>
+                <p>{message}</p>
             </div>
         );
     }
 });
 
+// Set variables(any type) as inputs for props
+var firstName = 'Ankit';
+var myMessage = 'Hey there! This is cruxbreaker.'
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName} message={myMessage}/>,        // Passing a prop into a component(Similar to adding a new HTML attribute)
     document.getElementById('app')
 );
